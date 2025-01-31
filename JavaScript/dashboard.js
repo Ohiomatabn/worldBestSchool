4387ab7b9a7c8eabeb85698d0eafec97bdd2ae77
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2025-01-31 14:18:19 +0100

    Create the arrays of the content in the dashboard

---
+++ JavaScript/dashboard.js
@@ -0,0 +1,25 @@
+export const dashboard = [{
+  name: 'My Profile',
+  image: 'my_profile.png'
+},{
+  name: 'Fee Allocation',
+  image: 'naira.png'
+},{
+  name: 'Fees Payment',
+  image: 'school_fees.png'
+},{
+  name: 'Outstanding Bills',
+  image: 'debt.png'
+},{
+  name: 'Result',
+  image: 'staffs.png'
+},{
+  name: 'Staffs',
+  image: 'staffs.png'
+},{
+  name: 'Transfer',
+  image: 'transfer.png'
+},{
+  name: 'Reset Password',
+  image: 'password.png'
+}];
