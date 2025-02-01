c734af7b923e7bdb642952e9a5473de21b8dbb43
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2025-02-01 16:59:54 +0100

    Add all the student details in an array of objects

---
+++ JavaScript/result.js
@@ -0,0 +1,282 @@
+const studentDetails = [{
+    name: 'John David',
+    term: '1st Term',
+    studentNumber : 'student0001',
+    result : {
+      score:{
+        english: 60,
+        maths: 50,
+        physcis: 64,
+        chemistry: 60,
+        biology: 72,
+        crs: 45,
+        civicEducation: 33,
+        agric: 90,
+        marketing: 83,
+      },
+      grade: {
+        english: 'B',
+        maths: 'C',
+        physcis: 'B',
+        chemistry: 'B',
+        biology: 'A',
+        crs: 'D7',
+        civicEducation: 'F9',
+        agric: 'A',
+        marketing: 'A',
+      }
+    }
+  },{
+    name: 'John David',
+    term: '1st Term',
+    studentNumber : 'student0002' ,
+    result : {
+      score:{
+        english: 60,
+        maths: 50,
+        physcis: 64,
+        chemistry: 60,
+        biology: 72,
+        crs: 45,
+        civicEducation: 33,
+        agric: 90,
+        marketing: 83,
+      },
+      grade: {
+        english: 'B',
+        maths: 'C',
+        physcis: 'B',
+        chemistry: 'B',
+        biology: 'A',
+        crs: 'D7',
+        civicEducation: 'F9',
+        agric: 'A',
+        marketing: 'A',
+      }
+    }
+    
+  },{
+    name: 'John David',
+    term: '1st Term',
+    studentNumber : 'student0003' ,
+    result : {
+      score:{
+        english: 60,
+        maths: 50,
+        physcis: 64,
+        chemistry: 60,
+        biology: 72,
+        crs: 45,
+        civicEducation: 33,
+        agric: 90,
+        marketing: 83,
+      },
+      grade: {
+        english: 'B',
+        maths: 'C',
+        physcis: 'B',
+        chemistry: 'B',
+        biology: 'A',
+        crs: 'D7',
+        civicEducation: 'F9',
+        agric: 'A',
+        marketing: 'A',
+      }
+    }
+  },{
+    name: 'John David',
+    term: '1st Term',
+    studentNumber : 'student0004',
+    result : {
+      score:{
+        english: 60,
+        maths: 50,
+        physcis: 64,
+        chemistry: 60,
+        biology: 72,
+        crs: 45,
+        civicEducation: 33,
+        agric: 90,
+        marketing: 83,
+      },
+      grade: {
+        english: 'B',
+        maths: 'C',
+        physcis: 'B',
+        chemistry: 'B',
+        biology: 'A',
+        crs: 'D7',
+        civicEducation: 'F9',
+        agric: 'A',
+        marketing: 'A',
+      }
+    }
+  },{
+    name: 'John David',
+    term: '1st Term',
+    studentNumber : 'student0005',
+    result : {
+      score:{
+        english: 60,
+        maths: 50,
+        physcis: 64,
+        chemistry: 60,
+        biology: 72,
+        crs: 45,
+        civicEducation: 33,
+        agric: 90,
+        marketing: 83,
+      },
+      grade: {
+        english: 'B',
+        maths: 'C',
+        physcis: 'B',
+        chemistry: 'B',
+        biology: 'A',
+        crs: 'D7',
+        civicEducation: 'F9',
+        agric: 'A',
+        marketing: 'A',
+      }
+    }
+  },{
+        name: 'John David',
+    term: '1st Term',
+      studentNumber : 'student0006',
+      result : {
+        score:{
+          english: 60,
+          maths: 50,
+          physcis: 64,
+          chemistry: 60,
+          biology: 72,
+          crs: 45,
+          civicEducation: 33,
+          agric: 90,
+          marketing: 83,
+        },
+        grade: {
+          english: 'B',
+          maths: 'C',
+          physcis: 'B',
+          chemistry: 'B',
+          biology: 'A',
+          crs: 'D7',
+          civicEducation: 'F9',
+          agric: 'A',
+          marketing: 'A',
+        }
+      }
+  },{
+        name: 'John David',
+    term: '1st Term',
+      studentNumber : 'student0007',
+      result : {
+        score:{
+          english: 60,
+          maths: 50,
+          physcis: 64,
+          chemistry: 60,
+          biology: 72,
+          crs: 45,
+          civicEducation: 33,
+          agric: 90,
+          marketing: 83,
+        },
+        grade: {
+          english: 'B',
+          maths: 'C',
+          physcis: 'B',
+          chemistry: 'B',
+          biology: 'A',
+          crs: 'D7',
+          civicEducation: 'F9',
+          agric: 'A',
+          marketing: 'A',
+        }
+      }
+  },{
+        name: 'John David',
+    term: '1st Term',
+      studentNumber : 'student0008',
+      result : {
+        score:{
+          english: 60,
+          maths: 50,
+          physcis: 64,
+          chemistry: 60,
+          biology: 72,
+          crs: 45,
+          civicEducation: 33,
+          agric: 90,
+          marketing: 83,
+        },
+        grade: {
+          english: 'B',
+          maths: 'C',
+          physcis: 'B',
+          chemistry: 'B',
+          biology: 'A',
+          crs: 'D7',
+          civicEducation: 'F9',
+          agric: 'A',
+          marketing: 'A',
+        }
+      }
+  },{
+      name: 'John David',
+      term: '1st Term',
+      studentNumber : 'student0009',
+      result : {
+        score:{
+          english: 60,
+          maths: 50,
+          physcis: 64,
+          chemistry: 60,
+          biology: 72,
+          crs: 45,
+          civicEducation: 33,
+          agric: 90,
+          marketing: 83,
+        },
+        grade: {
+          english: 'B',
+          maths: 'C',
+          physcis: 'B',
+          chemistry: 'B',
+          biology: 'A',
+          crs: 'D7',
+          civicEducation: 'F9',
+          agric: 'A',
+          marketing: 'A',
+        }
+      }
+  },{
+        name: 'John David',
+    term: '1st Term',
+      studentNumber : 'student0010',
+    result : {
+      score:{
+        english: 60,
+        maths: 50,
+        physcis: 64,
+        chemistry: 60,
+        biology: 72,
+        crs: 45,
+        civicEducation: 33,
+        agric: 90,
+        marketing: 83,
+      },
+      grade: {
+        english: 'B',
+        maths: 'C',
+        physcis: 'B',
+        chemistry: 'B',
+        biology: 'A',
+        crs: 'D7',
+        civicEducation: 'F9',
+        agric: 'A',
+        marketing: 'A',
+      }
+    }
+}];
