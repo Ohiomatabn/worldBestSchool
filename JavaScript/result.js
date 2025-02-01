a1b27ae8d4446e6f3919f88fa5c2fa23433bdc2d
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2025-02-01 18:09:36 +0100

    Create the displayResult fuctinon and call it when the check result button is clicked

--- JavaScript/result.js
+++ JavaScript/result.js
@@ -300,3 +300,129 @@ const studentDetails = [{
       }
     }
 }];
+
+
+
+
+function showResult (){
+  //Get the value of the input field
+  const examNumberInput = document.querySelector('#exam-no').value;
+  const selectedTerm = document.querySelector('#term').value;
+  const selectedYear = document.querySelector('#year').value;
+  let studentResult = '';
+  let resultHTML = '';
+
+  //using the value of the input field loop through the studentDetails array to find a matching one from the input field
+  studentDetails.forEach((studentDetail) =>{
+    if(studentDetail.studentNumber === examNumberInput && studentDetail.term === selectedTerm && studentDetail.year === selectedYear){
+      studentResult = studentDetail;
+      console.log(studentDetail.term)
+    } else{
+      console.log('Invalid')
+    }
+  });
+
+  //Display the result when a matching studentNumber is found
+  if(studentResult){
+    resultHTML = `
+                <div class="container">
+            <div class="border">
+              <div class="school-name">
+                <h2>World Best Secondary School</h2>
+                <h2>${studentResult.term} ${studentResult.year} Terminal Result</h2>
+              </div>
+              <div class="student-details">
+                <h2>${studentResult.name}</h2>
+                <hr />
+                <div class="flex">
+                  <div>
+                    <h2>${studentResult.studentNumber}</h2>
+                    <h5>Student Exam No</h4>
+                  </div>
+                  <div>
+                    <h2>${studentResult.term}</h2>
+                    <h5>${studentResult.currentClass}</h4>
+                  </div>
+                  <div>
+                    <img src="../Student_Profile_Image/Snapchat-1599006824~2.jpg" alt="" class="passport">
+                  </div>
+                </div>
+              </div>
+              <hr>
+              <table>
+                <tr>
+                  <th>S/N</th>
+                  <th>Subject</th>
+                  <th>Score</th>
+                  <th>Grade</th>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>English</td>
+                  <td>${studentResult.result.score.english}</td>
+                  <td>${studentResult.result.grade.english}</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Mathematics</td>
+                  <td>${studentResult.result.score.maths}</td>
+                  <td>${studentResult.result.grade.maths}</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Physics</td>
+                  <td>${studentResult.result.score.physcis}</td>
+                  <td>${studentResult.result.grade.physcis}</td>
+                </tr>
+                <tr>
+                  <td>4</td>
+                  <td>Biology</td>
+                  <td>${studentResult.result.score.biology}</td>
+                  <td>${studentResult.result.grade.biology}</td>
+                </tr>
+                <tr>
+                  <td>5</td>
+                  <td>Chemistry</td>
+                  <td>${studentResult.result.score.chemistry}</td>
+                  <td>${studentResult.result.grade.chemistry}</td>
+                </tr>
+                <tr>
+                  <td>6</td>
+                  <td>Civic Education</td>
+                  <td>${studentResult.result.score.civicEducation}</td>
+                  <td>${studentResult.result.grade.civicEducation}</td>
+                </tr>
+                <tr>
+                  <td>7</td>
+                  <td>CRS</td>
+                  <td>${studentResult.result.score.crs}</td>
+                  <td>${studentResult.result.grade.crs}</td>
+                </tr>
+                <tr>
+                  <td>8</td>
+                  <td>Agric Science</td>
+                  <td>${studentResult.result.score.agric}</td>
+                  <td>${studentResult.result.grade.agric}</td>
+                </tr>
+                <tr>
+                  <td>9</td>
+                  <td>Marketing</td>
+                  <td>${studentResult.result.score.marketing}</td>
+                  <td>${studentResult.result.grade.marketing}</td>
+                </tr>
+              </table>
+            </div>
+          </div>
+    `
+  } else{
+    resultHTML = 'Invalid Parameter';
+  }
+
+
+  //updating the DOM
+  document.querySelector('.result').innerHTML = resultHTML;
+}
+
+
+//Add an event listner to the check result button
+document.querySelector('.check-result').addEventListener('click', showResult);
