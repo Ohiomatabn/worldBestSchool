5dde4414bb85218b8ce0663da6a8d5a0e069496e
Ohimata BN <benjaminohiomatanathaniel@gmail.com>
2025-02-03 20:01:47 +0100

    Create a new and seprate the studentDetails array from the result.js and import it

---
+++ JavaScript/resultDetails.js
@@ -0,0 +1,302 @@
+export const studentDetails = [{
+  name: 'John David',
+  term: 'First Term',
+  examNumber : 'student0001',
+  year: '2021/22',
+  currentClass: 'JSS 3',
+  result : {
+    score:{
+      english: 60,
+      maths: 50,
+      physcis: 64,
+      chemistry: 60,
+      biology: 72,
+      crs: 45,
+      civicEducation: 33,
+      agric: 90,
+      marketing: 83,
+    },
+    grade: {
+      english: 'B2',
+      maths: 'C4',
+      physcis: 'B2',
+      chemistry: 'B2',
+      biology: 'A1',
+      crs: 'D7',
+      civicEducation: 'F9',
+      agric: 'A1',
+      marketing: 'A1',
+    }
+  }
+},{
+  name: 'John David',
+  term: 'Second Term',
+  examNumber : 'student0002' ,
+  year: '2021/22',
+  currentClass: 'JSS 3',
+  result : {
+    score:{
+      english: 88,
+      maths: 72,
+      physcis: 74,
+      chemistry: 35,
+      biology: 77,
+      crs: 90,
+      civicEducation: 60,
+      agric: 80,
+      marketing: 93,
+    },
+    grade: {
+      english: 'A1',
+      maths: 'A1',
+      physcis: 'A1',
+      chemistry: 'F9',
+      biology: 'A1',
+      crs: 'A1',
+      civicEducation: 'B2',
+      agric: 'A1',
+      marketing: 'A1',
+    }
+  }
+  
+},{
+  name: 'John David',
+  term: 'Thrid Term',
+  examNumber : 'student0003' ,
+  year: '2021/22',
+  currentClass: 'JSS 3',
+  result : {
+    score:{
+      english: 55,
+      maths: 90,
+      physcis: 89,
+      chemistry: 77,
+      biology: 72,
+      crs: 95,
+      civicEducation: 72,
+      agric: 81,
+      marketing: 75,
+    },
+    grade: {
+      english: 'C4',
+      maths: 'A1',
+      physcis: 'A1',
+      chemistry: 'A1',
+      biology: 'A1',
+      crs: 'A1',
+      civicEducation: 'A1',
+      agric: 'A1',
+      marketing: 'A1',
+    }
+  }
+},{
+  name: 'John David',
+  term: 'First Term',
+  examNumber : 'student0004',
+  year: '2022/23',
+  currentClass: 'SSS 1',
+  result : {
+    score:{
+      english: 60,
+      maths: 62,
+      physcis: 69,
+      chemistry: 70,
+      biology: 87,
+      crs: 92,
+      civicEducation: 55,
+      agric: 91,
+      marketing: 63,
+    },
+    grade: {
+      english: 'B2',
+      maths: 'B2',
+      physcis: 'B2',
+      chemistry: 'A1',
+      biology: 'A1',
+      crs: 'A1',
+      civicEducation: 'C4',
+      agric: 'A1',
+      marketing: 'B2',
+    }
+  }
+},{
+  name: 'John David',
+  term: 'Second Term',
+  examNumber : 'student0005',
+  year: '2022/23',
+  currentClass: 'SSS 1',
+  result : {
+    score:{
+      english: 69,
+      maths: 60,
+      physcis: 64,
+      chemistry: 63,
+      biology: 67,
+      crs: 69,
+      civicEducation: 61,
+      agric: 60,
+      marketing: 68,
+    },
+    grade: {
+      english: 'B2',
+      maths: 'B2',
+      physcis: 'B2',
+      chemistry: 'B2',
+      biology: 'B2',
+      crs: 'B2',
+      civicEducation: 'B2',
+      agric: 'B2',
+      marketing: 'B2',
+    }
+  }
+},{
+  name: 'John David',
+  term: 'Thrid Term',
+    examNumber : 'student0006',
+    year: '2022/23',
+    currentClass: 'SSS 1',
+    result : {
+      score:{
+        english: 80,
+        maths: 45,
+        physcis: 65,
+        chemistry: 80,
+        biology: 70,
+        crs: 95,
+        civicEducation: 40,
+        agric: 91,
+        marketing: 55,
+      },
+      grade: {
+        english: 'A1',
+        maths: 'D7',
+        physcis: 'B2',
+        chemistry: 'A1',
+        biology: 'A1',
+        crs: 'A1',
+        civicEducation: 'E8',
+        agric: 'A1',
+        marketing: 'C4',
+      }
+    }
+},{
+  name: 'John David',
+  term: 'First Term',
+    examNumber : 'student0007',
+    year: '2023/24',
+    currentClass: 'SSS 2',
+    result : {
+      score:{
+        english: 92,
+        maths: 82,
+        physcis: 78,
+        chemistry: 90,
+        biology: 88,
+        crs: 95,
+        civicEducation: 80,
+        agric: 92,
+        marketing: 83,
+      },
+      grade: {
+        english: 'A1',
+        maths: 'A1',
+        physcis: 'A1',
+        chemistry: 'A1',
+        biology: 'A1',
+        crs: 'A1',
+        civicEducation: 'A1',
+        agric: 'A1',
+        marketing: 'A1',
+      }
+    }
+},{
+  name: 'John David',
+  term: 'Second Term',
+    examNumber : 'student0008',
+    year: '2023/24',
+    currentClass: 'SSS 2',
+    result : {
+      score:{
+        english: 40,
+        maths: 77,
+        physcis: 79,
+        chemistry: 39,
+        biology: 72,
+        crs: 88,
+        civicEducation: 78,
+        agric: 67,
+        marketing: 77,
+      },
+      grade: {
+        english: 'E8',
+        maths: 'A1',
+        physcis: 'A1',
+        chemistry: 'F9',
+        biology: 'A1',
+        crs: 'A1',
+        civicEducation: 'A1',
+        agric: 'B2',
+        marketing: 'A1',
+      }
+    }
+},{
+    name: 'John David',
+    term: 'Thrid Term',
+    examNumber : 'student0009',
+    year: '2023/24',
+    currentClass: 'SSS 2',
+    result : {
+      score:{
+        english: 62,
+        maths: 52,
+        physcis: 66,
+        chemistry: 62,
+        biology: 74,
+        crs: 47,
+        civicEducation: 35,
+        agric: 92,
+        marketing: 85,
+      },
+      grade: {
+        english: 'B2',
+        maths: 'C4',
+        physcis: 'B2',
+        chemistry: 'B2',
+        biology: 'A1',
+        crs: 'D7',
+        civicEducation: 'F9',
+        agric: 'A1',
+        marketing: 'A1',
+      }
+    }
+},{
+  name: 'John David',
+  term: 'First Term',
+  examNumber : 'student0010',
+  year: '2024/25',
+  currentClass: 'SSS 3',
+  result : {
+    score:{
+      english: 70,
+      maths: 60,
+      physcis: 74,
+      chemistry: 70,
+      biology: 82,
+      crs: 95,
+      civicEducation: 43,
+      agric: 95,
+      marketing: 93,
+    },
+    grade: {
+      english: 'A1',
+      maths: 'B2',
+      physcis: 'A1',
+      chemistry: 'A1',
+      biology: 'A1',
+      crs: 'A1',
+      civicEducation: 'E8',
+      agric: 'A1',
+      marketing: 'A1',
+    }
+  }
+}];
